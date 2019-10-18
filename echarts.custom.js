76b128c3a76755778050fe87def7108105a6af49
wjxu <weijian.xuwj@01bees.com>
2019-10-18 17:14:52 +0800

    feat: 初始化项目

---
+++ echarts.custom.js
@@ -0,0 +1,104 @@
+// 引入 echarts 主模块。
+export * from 'echarts/src/echarts';
+// 引入饼图。
+import 'echarts/src/chart/pie';
+
+// 按需加载 echarts 提供的组件
+
+import 'echarts/src/component/dataset';
+
+import 'echarts/src/chart/line';
+
+import 'echarts/src/chart/bar';
+
+import 'echarts/src/chart/pie';
+// 散点图
+import 'echarts/src/chart/scatter';
+
+import 'echarts/src/chart/radar';
+
+import 'echarts/src/chart/map';
+
+// import 'echarts/src/chart/tree'
+
+// import 'echarts/src/chart/treemap'
+
+// import 'echarts/src/chart/graph'
+// 仪表盘
+import 'echarts/src/chart/gauge';
+
+// import 'echarts/src/chart/funnel'
+
+// import 'echarts/src/chart/parallel'
+
+// import 'echarts/src/chart/sankey'
+
+// import 'echarts/src/chart/boxplot'
+
+// import 'echarts/src/chart/candlestick'
+
+import 'echarts/src/chart/effectScatter';
+
+import 'echarts/src/chart/lines';
+
+import 'echarts/src/chart/heatmap';
+
+// import 'echarts/src/chart/pictorialBar'
+
+// import 'echarts/src/chart/themeRiver'
+
+// import 'echarts/src/chart/sunburst'
+
+// import 'echarts/src/chart/custom'
+
+import 'echarts/src/component/grid';
+
+import 'echarts/src/component/polar';
+
+import 'echarts/src/component/geo';
+
+import 'echarts/src/component/singleAxis';
+
+// import 'echarts/src/component/parallel'
+
+// import 'echarts/src/component/calendar'
+// 自定义图形
+// import 'echarts/src/component/graphic'
+
+import 'echarts/src/component/toolbox';
+
+import 'echarts/src/component/tooltip';
+// 坐标轴指示器配置项。
+import 'echarts/src/component/axisPointer';
+
+// import 'echarts/src/component/brush'
+
+import 'echarts/src/component/title';
+
+import 'echarts/src/component/timeline';
+
+import 'echarts/src/component/markPoint';
+
+import 'echarts/src/component/markLine';
+
+// import 'echarts/src/component/markArea'
+
+import 'echarts/src/component/legendScroll';
+
+import 'echarts/src/component/legend';
+
+import 'echarts/src/component/dataZoom';
+
+import 'echarts/src/component/dataZoomInside';
+
+import 'echarts/src/component/dataZoomSlider'
+
+// import 'echarts/src/component/visualMap'
+
+// import 'echarts/src/component/visualMapContinuous'
+
+// import 'echarts/src/component/visualMapPiecewise'
+
+import 'zrender/src/vml/vml';
+
+import 'zrender/src/svg/svg';
