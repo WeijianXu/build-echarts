8837dfee70ed75be6f1c96619c0b3fdb2d1c8cc9
xuwj <xwj248062@alibaba-inc.com>
2022-12-02 20:43:29 +0800

    feat: build charts

--- echarts.custom.js
+++ echarts.custom.js
@@ -1,7 +1,5 @@
 // 引入 echarts 主模块。
 export * from 'echarts/src/echarts';
-// 引入饼图。
-import 'echarts/src/chart/pie';
 
 // 按需加载 echarts 提供的组件
 
@@ -19,23 +17,23 @@ import 'echarts/src/chart/radar';
 
 import 'echarts/src/chart/map';
 
-// import 'echarts/src/chart/tree'
+import 'echarts/src/chart/tree'
 
-// import 'echarts/src/chart/treemap'
+import 'echarts/src/chart/treemap'
 
-// import 'echarts/src/chart/graph'
+import 'echarts/src/chart/graph'
 // 仪表盘
 import 'echarts/src/chart/gauge';
 
-// import 'echarts/src/chart/funnel'
+import 'echarts/src/chart/funnel'
 
-// import 'echarts/src/chart/parallel'
+import 'echarts/src/chart/parallel'
 
-// import 'echarts/src/chart/sankey'
+import 'echarts/src/chart/sankey'
 
-// import 'echarts/src/chart/boxplot'
+import 'echarts/src/chart/boxplot'
 
-// import 'echarts/src/chart/candlestick'
+import 'echarts/src/chart/candlestick'
 
 import 'echarts/src/chart/effectScatter';
 
@@ -43,13 +41,13 @@ import 'echarts/src/chart/lines';
 
 import 'echarts/src/chart/heatmap';
 
-// import 'echarts/src/chart/pictorialBar'
+import 'echarts/src/chart/pictorialBar'
 
-// import 'echarts/src/chart/themeRiver'
+import 'echarts/src/chart/themeRiver'
 
-// import 'echarts/src/chart/sunburst'
+import 'echarts/src/chart/sunburst'
 
-// import 'echarts/src/chart/custom'
+import 'echarts/src/chart/custom'
 
 import 'echarts/src/component/grid';
 
@@ -59,11 +57,11 @@ import 'echarts/src/component/geo';
 
 import 'echarts/src/component/singleAxis';
 
-// import 'echarts/src/component/parallel'
+import 'echarts/src/component/parallel'
 
-// import 'echarts/src/component/calendar'
+import 'echarts/src/component/calendar'
 // 自定义图形
-// import 'echarts/src/component/graphic'
+import 'echarts/src/component/graphic'
 
 import 'echarts/src/component/toolbox';
 
@@ -71,7 +69,7 @@ import 'echarts/src/component/tooltip';
 // 坐标轴指示器配置项。
 import 'echarts/src/component/axisPointer';
 
-// import 'echarts/src/component/brush'
+import 'echarts/src/component/brush'
 
 import 'echarts/src/component/title';
 
@@ -81,7 +79,7 @@ import 'echarts/src/component/markPoint';
 
 import 'echarts/src/component/markLine';
 
-// import 'echarts/src/component/markArea'
+import 'echarts/src/component/markArea'
 
 import 'echarts/src/component/legendScroll';
 
@@ -93,11 +91,11 @@ import 'echarts/src/component/dataZoomInside';
 
 import 'echarts/src/component/dataZoomSlider'
 
-// import 'echarts/src/component/visualMap'
+import 'echarts/src/component/visualMap'
 
-// import 'echarts/src/component/visualMapContinuous'
+import 'echarts/src/component/visualMapContinuous'
 
-// import 'echarts/src/component/visualMapPiecewise'
+import 'echarts/src/component/visualMapPiecewise'
 
 import 'zrender/src/vml/vml';
 
